fb5ccf140ab72d2caceea247b4a2e9687a55f08c
Thế Công <phamthecong198@gmail.com>
2022-05-13 01:09:27 +0700

    .

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -63,7 +63,7 @@ const Item = ({ title, icon }) => (
 
 const Login = ({ route, navigation }) => {
   const dispatch = useDispatch();
-  const [userName, setUsername] = useState("hanoifund");
+  const [userName, setUsername] = useState("AB0029019");
   const [passWord, setPassWord] = useState("123");
   const [checkEye, setCheckEye] = useState(true);
   const [checkUser, setCheckUser] = useState(false)
